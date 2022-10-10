afd6e57cd8aca866311f0a35202e8d29ea74b567
NusratJahanBristy <hellonusratjahanbristy@gmail.com>
2022-10-11 02:55:33 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
-
+import Navbars from './components/Navbar/Navbar';
 function App() {
   
   return (
     <div className="App">
      
-    
+    <Navbars></Navbars>
     </div>
   );
 }
