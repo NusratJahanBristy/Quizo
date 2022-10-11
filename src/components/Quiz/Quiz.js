a4d50a82f5d0d54d5fc9264b3f0a660bf1778692
NusratJahanBristy <hellonusratjahanbristy@gmail.com>
2022-10-12 02:22:41 +0600

    code formating

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -21,7 +21,7 @@ const Quiz = ({ quiz }) => {
     return (
         <div className='bg-success  m-5 p-5 text-white '>
             <div className='d-flex flex-coloum justify-content-between align-items-center'>
-                <h4>id:{question.slice(3,-4)}</h4>
+                <h4>{question.slice(3,-4)}</h4>
                 <FontAwesomeIcon onClick={() => showAnswer(correctAnswer)} className=' ' icon={faEye} />
             </div>
             <div className='row row-cols-2 mt-5 g-3'>
